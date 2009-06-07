4f2071f0f3e04e82c91fcf135c04ea62ec478ac1
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2009-06-07 04:21:44 +0100

    Mapped controls to Remote/Classic Controller

--- src/joystick.c
+++ src/joystick.c
@@ -2,8 +2,8 @@
     C-Dogs SDL
     A port of the legendary (and fun) action/arcade cdogs.
     Copyright (C) 1995 Ronny Wester
-    Copyright (C) 2003 Jeremy Chin 
-    Copyright (C) 2003-2007 Lucas Martin-King 
+    Copyright (C) 2003 Jeremy Chin
+    Copyright (C) 2003-2007 Lucas Martin-King
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -21,20 +21,19 @@
 
 -------------------------------------------------------------------------------
 
- joystick.c - um... you know what it is 
- 
+ joystick.c - um... you know what it is
+
  Author: $Author: lmartinking $
  Rev:    $Revision: 250 $
  URL:    $HeadURL: svn://svn.icculus.org/cdogs-sdl/trunk/src/joystick.c $
  ID:     $Id: joystick.c 250 2007-07-06 16:38:43Z lmartinking $
- 
+
 */
 
 #include <string.h>
 #include <stdio.h>
-// #include <conio.h>
-// #include <i86.h>
 #include "joystick.h"
+#include "wii.h"
 
 /* some of this stuff could be better, but it works! */
 
@@ -55,19 +54,30 @@ void PollSticks(int maxWait)
 			int i;
 			int max;
 			int btn = 0;
-			
+
 			j = gSticks[idx].j;
-			gSticks[idx].x = SDL_JoystickGetAxis(j, 0);
-			gSticks[idx].y = SDL_JoystickGetAxis(j, 1);
+			Uint8 hat = SDL_JoystickGetHat(j, 0);
+			if ((hat & SDL_HAT_RIGHT) != 0)
+				gSticks[idx].x = 1;
+			else if ((hat & SDL_HAT_LEFT) != 0)
+				gSticks[idx].x = -1;
+			else
+				gSticks[idx].x = 0;
 
-			max = gSticks[idx].nr_buttons;
-			if (max > 4) max = 4;
+			if ((hat & SDL_HAT_UP) != 0)
+				gSticks[idx].y = 1;
+			else if ((hat & SDL_HAT_DOWN) != 0)
+				gSticks[idx].y = -1;
+			else
+				gSticks[idx].y = 0;
 
-			for (i = 0; i < max; i++) {
-				if (SDL_JoystickGetButton(j, i)) {
-					btn |= (2^i);
-				}
-			}
+			if (SDL_JoystickGetButton(j, REMOTE_1) ||
+				SDL_JoystickGetButton(j, CLASSIC_B))
+				btn |= 1;
+
+			if (SDL_JoystickGetButton(j, REMOTE_2) ||
+				SDL_JoystickGetButton(j, CLASSIC_A))
+				btn |= 2;
 
 			gSticks[idx].buttons = btn;
 		}
@@ -78,7 +88,7 @@ void InitSticks(void)
 {
 	int i;
 	int n;
-	
+
 	gSticks[0].present = NO;
 	gSticks[1].present = NO;
 	gSticks[0].inUse = NO;
@@ -103,14 +113,14 @@ void InitSticks(void)
 			SDL_JoystickClose(j);
 			j = NULL;
 		}
-		
+
 		j = SDL_JoystickOpen(i);
 
 		if (j) {
 			int nb, na;
 			nb = SDL_JoystickNumButtons(j);
 			na = SDL_JoystickNumAxes(j);
-			
+
 			printf("Opened Joystick %d\n", i);
 			printf(" -> %s\n", SDL_JoystickName(i));
 			printf(" -> Axes: %d Buttons: %d\n", na, nb);
@@ -155,15 +165,15 @@ void PollDigiSticks(int *joy1, int *joy2)
 	if (joy1)
 		*joy1 = 0;
 	if (joy1 && gSticks[0].present) {
-		if (gSticks[0].x < (gSticks[0].xMid - js1_threshold)) {
+		if (gSticks[0].x < 0) {
 			*joy1 |= JOYSTICK_LEFT;
-		} else if (gSticks[0].x > (gSticks[0].xMid + js1_threshold)) {
+		} else if (gSticks[0].x > 0) {
 			*joy1 |= JOYSTICK_RIGHT;
 		}
-		
-		if (gSticks[0].y < (gSticks[0].yMid - js1_threshold)) {
+
+		if (gSticks[0].y > 0) {
 			*joy1 |= JOYSTICK_UP;
-		} else if (gSticks[0].y > (gSticks[0].yMid + js1_threshold)) {
+		} else if (gSticks[0].y < 0) {
 			*joy1 |= JOYSTICK_DOWN;
 		}
 
@@ -179,15 +189,15 @@ void PollDigiSticks(int *joy1, int *joy2)
 	if (joy2)
 		*joy2 = 0;
 	if (joy2 && gSticks[1].present) {
-		if (gSticks[1].x < gSticks[1].xMid - js2_threshold) {
+		if (gSticks[1].x < 0) {
 			*joy2 |= JOYSTICK_LEFT;
-		} else if (gSticks[1].x > gSticks[1].xMid + js2_threshold) {
+		} else if (gSticks[1].x > 0) {
 			*joy2 |= JOYSTICK_RIGHT;
 		}
-		
-		if (gSticks[1].y < gSticks[1].yMid - js2_threshold) {
+
+		if (gSticks[1].y > 0) {
 			*joy2 |= JOYSTICK_UP;
-		} else if (gSticks[1].y > gSticks[1].yMid + js2_threshold) {
+		} else if (gSticks[1].y < 0) {
 			*joy2 |= JOYSTICK_DOWN;
 		}
 
